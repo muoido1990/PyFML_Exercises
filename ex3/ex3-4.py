e48d0aa8e377a2b5bdb8a2747c3c7d8018280bc8
Muoi <muoido1990@gmail.com>
2017-05-31 09:34:56 +0700

    test

---
+++ ex3/ex3-4.py
@@ -0,0 +1,26 @@
+'''
+- input_data = ["I", "Love", "You", "Chiu", "Chiu"]
+
+- output: in ra thành cặp
+
+Ví dụ::
+
+  1 I
+  2 Love
+  3 You
+  ... cho đến hết
+
+Gợi ý: có thể dùng enumerate()
+https://docs.python.org/2/library/functions.html#enumerate
+'''
+
+input_data = ['I', 'Love', 'You', 'Chiu', 'Chiu']
+L = list(enumerate(input_data, start = 1))
+for i in L:
+	print(i[0],i[1])
+	
+
+	
+	
+	
+	
